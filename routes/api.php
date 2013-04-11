ea860952be611ea6d36a2b82c3f6a55558d1a869
Elystus Black <elystus@gmail.com>
2013-04-10 20:34:32 -0700

    Improved api.php
    Made this file more dynamic and flexible

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
  * Service Id (You can get a service id here: https://census.soe.com/)
  * @var string
  */
-Flight::set('sid', '');
+Flight::set('sid', 's:elystus');
 
 /*
  * Sony Game (By default this is ps2)
@@ -18,23 +18,80 @@ Flight::set('sg', 'ps2');
 Flight::set('surl', 'http://census.soe.com/');
 
 /*
+ * Set array of valid types
+ * @var array
+ */
+Flight::set('stypes', array(
+    'character',
+    'characters_friend',
+    'characters_online_status',
+    'event',
+    'characters_event',
+    'characters_event_grouped',
+    'world',
+    'characters_world',
+    'map',
+    'indarmap',
+    'amerishmap',
+    'esamirmap',
+    'leaderboard',
+    'outfit',
+    'outfit_member'
+));
+
+Flight::set('sid', 's:elystus');
+
+/*
+ * Starts the api data fetching process
+ * @return array
+ */
+Flight::map('apiStart', function($verb, $type, $action) {
+    Flight::apiVerifyVerb($verb);
+    Flight::apiVerifyType($type);
+    $url = Flight::apiConstructUrl($verb, $type, $action);
+    $apiData = Flight::apiFetchData($url);
+    return $apiData;
+});
+
+/*
+ * Check to see if supplied verb is valid
+ * @return boolean
+ */
+Flight::map('apiVerifyVerb', function($verb) {
+    if(strtolower($verb) == 'get' || strtolower($verb) == 'count') {
+        return true;
+    } else {
+        $msg = 'The verb that was supplied is not valid. Supplied verb: {' . $verb . '}';
+        Flight::view()->set('msg', $msg);
+        Flight::render('error');
+        Flight::stop();
+    }
+});
+
+/*
+ * Check to see if supplied type is valid
+ * @return boolean
+ */
+Flight::map('apiVerifyType', function($type) {
+    if(in_array(strtolower($type), Flight::get('stypes'))) {
+        return true;
+    } else {
+        $msg = 'The type that was supplied is not valid. Supplied verb: {' . $type . '}';
+        Flight::view()->set('msg', $msg);
+        Flight::render('error');
+        Flight::stop();
+    }
+});
+
+/*
  * Fetch data using SOE API
  * @return array
  */
-Flight::map('apiFetchData', function($verb, $action) {
+Flight::map('apiFetchData', function($url) {
     
-    $url = Flight::apiConstructUrl($verb, $action);
+    $result = file_get_contents($url,0,null,null);
     
-    $ch = curl_init($url);
-    
-    $options = array(
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_HTTPHEADER => array('Content-type: application/json') ,
-    );
-    curl_setopt_array( $ch, $options );
-    $result =  curl_exec($ch); 
-    
-    $obj = Flight::apiToArray(json_decode($result)); // Convert the data we fetched to an array
+    $obj = json_decode($result, true);              // Convert the data we fetched to an array
     
     return $obj;
 });
@@ -60,20 +117,36 @@ Flight::map('apiToArray', function($obj) {
  * Construct url for interaction with SOE API
  * @return string
  */
-Flight::map('apiConstructUrl', function($verb, $action) {
-    if( strtolower($verb) == 'get' || strtolower($verb) == 'action') {
-        $url .= Flight::get('surl');
-        $url .= Flight::get('sid');
-        $url .= '/' . $verb . '/?';
-        foreach($action as $k => $a) {
+Flight::map('apiConstructUrl', function($verb, $type, $action) {
+    $url = Flight::get('surl');         // Url for sony's api
+
+    $url .= Flight::get('sid') . '/';   // Adds our station id to the link
+    $url .= $verb . '/';                // Adds our verb to the link (get or count)
+    $url .= Flight::get('sg') . '/';    // Adds the game we are fetching data for to the link
+    $url .= $type . '/?';
+    
+    $action = new CachingIterator(new ArrayIterator($action)); // Allows us to look for the last iteration of the foreach statement
+    
+    foreach($action as $k => $a) {
+        if ($action->hasNext()) {
             $url .= $k . '=' . $a . '&';
+        } else {
+            $url .= $k . '=' . $a;
         }
-        return $url;
-    } else {
-        $msg = 'There was an error in the API url construction. `' . $verb. '` was not identified as a valid verb';
+    }
+    return $url;
+});
+
+Flight::map('checkServiceID', function() {
+    $sid = Flight::get('sid');
+    if(empty($sid)) {
+        $msg = 'There was no service ID defined! A service ID needs to be defined in order for server requests to be made. Please add your service ID to api.php.';
         Flight::view()->set('msg', $msg);
         Flight::render('error');
-        return false;
+        Flight::stop();
+    } else {
+        return true;
     }
 });
+
 ?>
